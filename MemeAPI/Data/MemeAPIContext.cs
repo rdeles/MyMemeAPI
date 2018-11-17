8b0577bf38fa32a8c94e43ce5e9e77913cc46444
rdeles <riccideles@gmail.com>
2018-11-18 11:16:16 +1300

    First Commit

---
+++ MemeAPI/Data/MemeAPIContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MemeAPI.Models
+{
+    public class MemeAPIContext : DbContext
+    {
+        public MemeAPIContext (DbContextOptions<MemeAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MemeAPI.Models.MemeItem> MemeItem { get; set; }
+    }
+}
